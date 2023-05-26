13b234b4050ecd36e6e85e3a8b81d6a6af5b204e
Angel Chaidez <angelomarchaidez@gmail.com>
2023-05-26 11:16:42 -0700

    project week 20 completed

---
+++ Jenkins_EC2/outputs.tf
@@ -0,0 +1,16 @@
+output "_1-Jenkins-project" {
+  description = "Print out to screen"
+  value       = "Hello from your Jenkins-Terraform-AWS project"
+}
+output "_2-Public_url" {
+  description = "Public URL for our Web Server"
+  value       = "http://${aws_instance.Jenkins_EC2_Instance.public_ip}:8080/index.html"
+}
+output "_3-SSH_ec2_instance" {
+  description = "Public IP for SSH-EC2-Instance login"
+  value       = "ssh -i JenkinsCICD.pem ec2-user@${aws_instance.Jenkins_EC2_Instance.public_ip}"
+}
+output "_4-Initial_Command" {
+  description = "Run this inital command to retrieve default password"
+  value       = "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+}
